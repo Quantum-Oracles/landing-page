47b1c7fabd1e7b511f7f9c1bdf8a7b88788e81bf
Yash Goyal <yashgo0018@gmail.com>
2023-12-21 22:51:28 +0530

    fixed the typeform

--- src/pages/HomePage/components/Hero.tsx
+++ src/pages/HomePage/components/Hero.tsx
@@ -4,6 +4,7 @@ import usePosition from "../../../hooks/usePosition";
 import { useEffect, useRef, useState } from "react";
 import { clampValue } from "../../../utils";
 import { Link } from "react-router-dom";
+import { PopupButton } from "@typeform/embed-react";
 
 export default function Hero() {
   const contentRef = useRef() as React.MutableRefObject<HTMLDivElement>;
@@ -75,12 +76,13 @@ export default function Hero() {
         </p>
 
         <div className="flex gap-x-20">
-          <button
+          <PopupButton
+            id="xeMPj4UL"
             className="bg-gradient-to-br from-secondary text-front tracking-wider font-raleway font-semibold to-transparent rounded-full px-10 py-4 overflow-hidden relative hover:opacity-70
         before:absolute-cover before:bg-secondary before:opacity-10 before:-z-1 duration-500 before:duration-inherit hover:before:opacity-100 hover:text-back hover:scale-90"
           >
             Get Notified
-          </button>
+          </PopupButton>
           <Link
             to="/developers/docs"
             className="bg-gradient-to-bl from-primary text-front tracking-wider font-raleway font-semibold to-transparent rounded-full px-10 py-4 overflow-hidden relative hover:opacity-70
