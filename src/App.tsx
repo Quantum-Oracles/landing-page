bf70eee0e13aaa2a48f708b866f5c0ff80f4e7cf
Yash Goyal <yashgo0018@gmail.com>
2023-12-21 22:55:09 +0530

    removed unneccessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { GlobalContextProvider } from "./contexts/globalContext";
 import router from "./pages/router";
-import { Web3Provider } from "./contexts/web3context";
 
 export default function App() {
   return (
     <GlobalContextProvider>
-      <Web3Provider>
-        <RouterProvider router={router} />
-      </Web3Provider>
+      <RouterProvider router={router} />
     </GlobalContextProvider>
     // </WagmiConfig>
   );
